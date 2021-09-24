a364e56be825456fb191c0916ebe581c22c565b7
jingcqu <jingchun.qu.work@gmail.com>
2021-09-24 16:50:03 -0400

    Comfigured MongoEmbeddedProfile,
    Can now use MongoDb

--- src/main/java/com/coasters/stats/repository/CourseRepository.java
+++ src/main/java/com/coasters/stats/repository/CourseRepository.java
@@ -1,10 +1,11 @@
 package com.coasters.stats.repository;
 
+import com.coasters.stats.domain.Course;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 /**
  * author: jingcqu
  * on: 24/09/2021
  */
-public interface CourseRepository extends ReactiveMongoRepository {
+public interface CourseRepository extends ReactiveMongoRepository<Course, String> {
 }
