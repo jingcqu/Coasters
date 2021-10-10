834696393f39f633b69f0c2ae737af8e849dbd30
jingcqu <jingchun.qu.work@gmail.com>
2021-10-10 04:41:25 -0400

    Replaced dependencies as they don't work with the latest versions of swagger
    Still added config file, maybe come in handy later

---
+++ src/main/java/com/coasters/stats/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.coasters.stats.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurationSupport;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+/**
+ * author: jingcqu
+ * on: 10/10/2021
+ */
+
+
+@Configuration
+public class SwaggerConfig implements WebFluxConfigurer {
+    @Bean
+    public Docket apiDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+}
