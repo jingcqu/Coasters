ee058c3f23f0bd6504425d03e4ca7804705d10f9
jingcqu <jingchun.qu.work@gmail.com>
2021-10-10 04:08:38 -0400

    Need to add bootstrap for testing

---
+++ src/main/java/com/coasters/stats/BootStrap/LoadData.java
@@ -0,0 +1,15 @@
+package com.coasters.stats.BootStrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * author: jingcqu
+ * on: 10/10/2021
+ */
+
+public class LoadData implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
