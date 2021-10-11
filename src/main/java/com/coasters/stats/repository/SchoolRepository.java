6bac82e1777d4d25e835af23eb84c0df0d881b79
jingcqu <jingchun.qu.work@gmail.com>
2021-10-11 03:41:16 -0400

    Fixed School Repository

--- src/main/java/com/coasters/stats/repository/SchoolRepository.java
+++ src/main/java/com/coasters/stats/repository/SchoolRepository.java
@@ -2,6 +2,7 @@ package com.coasters.stats.repository;
 
 import com.coasters.stats.domain.School;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
@@ -10,5 +11,5 @@ import java.util.Optional;
  * on: 24/09/2021
  */
 public interface SchoolRepository extends ReactiveMongoRepository<School, String> {
-    Optional<School> findByName(String name);
+    Mono<School> findByName(String name);
 }
